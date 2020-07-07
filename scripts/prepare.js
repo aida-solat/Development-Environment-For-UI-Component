2fe459618b4d3fe1b65e8364ffd9280192a65665
Norbert de Langen <ndelangen@me.com>
2020-07-07 14:21:54 +0200

    REMOVE the cleanup phase
    the cleanup is a signal that we're doing too much work transpiling / copying

--- scripts/prepare.js
+++ scripts/prepare.js
@@ -27,41 +27,6 @@ const ignore = [
   /.+\.test\..+/,
 ];
 
-function cleanup() {
-  // remove files after babel --copy-files output
-  // --copy-files option doesn't work with --ignore
-  // https://github.com/babel/babel/issues/6226
-  if (fs.existsSync(path.join(process.cwd(), 'dist'))) {
-    const files = shell.find('dist').filter((filePath) => {
-      // Do not remove folder
-      // And do not clean anything for:
-      // - @storybook/cli/dist/generators/**/template*
-      // - @storybook/cli/dist/frameworks/*
-      // because these are the template files
-      // that will be copied to init SB on users' projects
-      if (
-        fs.lstatSync(filePath).isDirectory() ||
-        /generators\/.+\/template.*/.test(filePath) ||
-        /dist\/frameworks\/.*/.test(filePath)
-      ) {
-        return false;
-      }
-
-      // Remove all copied TS files (but not the .d.ts)
-      if (/\.tsx?$/.test(filePath) && !/\.d\.ts$/.test(filePath)) {
-        return true;
-      }
-
-      return ignore.reduce((acc, pattern) => {
-        return acc || !!filePath.match(pattern);
-      }, false);
-    });
-    if (files.length) {
-      shell.rm('-f', ...files);
-    }
-  }
-}
-
 function logError(type, packageJson, errorLogs) {
   log.error(`FAILED (${type}) : ${errorLogs}`);
   log.error(
@@ -75,7 +40,5 @@ removeDist();
 
 babelify({ errorCallback: (errorLogs) => logError('js', packageJson, errorLogs) });
 tscfy({ errorCallback: (errorLogs) => logError('ts', packageJson, errorLogs) });
-
-cleanup();
 
 console.log(chalk.gray(`Built: ${chalk.bold(`${packageJson.name}@${packageJson.version}`)}`));
